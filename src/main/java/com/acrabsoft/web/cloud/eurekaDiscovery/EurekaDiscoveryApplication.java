97d2fdb25d6c8e886852357e3a57f43b417a0273
dengyifei789 <260721762@qq.com>
2018-11-06 22:34:34 +0800

    client

---
+++ src/main/java/com/acrabsoft/web/cloud/eurekaDiscovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,20 @@
+package com.acrabsoft.web.cloud.eurekaDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EurekaDiscoveryApplication {
+
+	@RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+	 
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+}
