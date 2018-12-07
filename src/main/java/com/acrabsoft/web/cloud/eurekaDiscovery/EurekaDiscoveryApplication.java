1e548d1eb65c22ecf97d948f925247fb715a137c
dengyifei <260721762@qq.com>
2018-12-07 17:20:57 +0800

    client

--- src/main/java/com/acrabsoft/web/cloud/eurekaDiscovery/EurekaDiscoveryApplication.java
+++ src/main/java/com/acrabsoft/web/cloud/eurekaDiscovery/EurekaDiscoveryApplication.java
@@ -2,6 +2,7 @@ package com.acrabsoft.web.cloud.eurekaDiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
